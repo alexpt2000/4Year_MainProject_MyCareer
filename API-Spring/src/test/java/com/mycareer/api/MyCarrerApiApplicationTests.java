14cb8e6d99ae603de6ca4bb61d3ffd03e91f22e9
alexpt2000gmit <g00317835@gmit.ie>
2018-01-21 19:50:02 +0000

    Update the Test File

--- API-Spring/src/test/java/com/mycareer/api/MyCarrerApiApplicationTests.java
+++ API-Spring/src/test/java/com/mycareer/api/MyCarrerApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.algamoney.api;
+package com.mycareer.api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AlgamoneyApiApplicationTests {
+public class MyCarrerApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
